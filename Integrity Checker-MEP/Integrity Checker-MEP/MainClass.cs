23c29f47ca23c0a2e1b5ed5b2a4360b0700393b2
Hyuni02 <jackjsh@naver.com>
2023-12-20 17:47:20 +0900

    버튼 이름 변경

--- Integrity Checker-MEP/Integrity Checker-MEP/MainClass.cs
+++ Integrity Checker-MEP/Integrity Checker-MEP/MainClass.cs
@@ -11,11 +11,11 @@ namespace Integrity_Checker_MEP
     [Plugin("Integrity Checker-MEP", "LC", DisplayName = "Integrity Checker-MEP")]
     [RibbonLayout("Integrity_Checker_MEP.xaml")]
     [RibbonTab("IntegrityCheckerTab")]
-    [Command("Button_One", Icon = "1_16.png", LargeIcon = "1_32.png")]
-    [Command("Button_Two", Icon = "2_16.png", LargeIcon = "2_32.png")]
-    [Command("Split_Button_1")]
-    [Command("Button_Three", Icon = "3_16.png", LargeIcon = "3_32.png")]
-    [Command("Button_Four", Icon = "4_16.png", LargeIcon = "4_32.png")]
+    [Command("Clash_Checker", Icon = "1_16.png", LargeIcon = "1_32.png")]
+    [Command("Result_Receiver", Icon = "2_16.png", LargeIcon = "2_32.png")]
+    [Command("Split_Button")]
+    [Command("Clash_Checker_split", Icon = "3_16.png", LargeIcon = "3_32.png")]
+    [Command("Result_Receiver_split", Icon = "4_16.png", LargeIcon = "4_32.png")]
     public class MainClass : CommandHandlerPlugin
     {
         private Form1 form1;
@@ -27,50 +27,56 @@ namespace Integrity_Checker_MEP
         {
             switch (name)
             {
-                case "Button_One":
-                    if(form1 == null)
-                    {
+                case "Clash_Checker":
+                    //if(mainFunc == null)
+                    //{
                         mainFunc = new MainFunc();
                         mainFunc.Execute();
 
                         //form1 = new Form1();
                         //form1.Show();
                         //form1.FormClosed += (s, args) => form1 = null;
-                    }
-                    else
-                    {
-                        //todo 닫기 구현
-                        //form1.Close();
-                        //form1 = null;
-                    }
+                    //}
+                    //else
+                    //{
+                    //    //todo 닫기 구현
+                    //    //form1.Close();
+                    //    //form1 = null;
+                    //    mainFunc = null;
+                    //}
                     break;
-                case "Button_Two":
-                    if (form2 == null)
-                    {
+                case "Result_Receiver":
+                    if (mainClass2 == null) {
                         mainClass2 = new MainClass2();
                         mainClass2.Execute();
 
                         //form2 = new Form2();
                         //form2.Show();
                         //form2.FormClosed += (s, args) => form2 = null;
-
                     }
-                    else
-                    {
+                    else {
                         //todo 닫기 구현
                         //form2.Close();
                         //form2 = null;
+
+                        mainClass2 = null;
                     }
                     break;
-                /*case "Button_Three":
-                    MessageBox.Show("In three words I can sum up everything I've learned" +
-                        " about life: it goes on.\n-Robert Frost",
-                        "Sample - Button Three");
+                case "Clash_Checker_split":
+                    mainFunc = new MainFunc();
+                    mainFunc.Execute();
+
+                    //MessageBox.Show("In three words I can sum up everything I've learned" +
+                    //    " about life: it goes on.\n-Robert Frost",
+                    //    "Sample - Button Three");
                     break;
-                case "Button_Four":
-                    MessageBox.Show("Four things for success: work and pray, " +
-                        "think and believe.\n-Norman Vincent Peale", "Sample - Button Four");
-                    break;*/
+                case "Result_Receiver_split":
+                    mainClass2 = new MainClass2();
+                    mainClass2.Execute();
+
+                    //MessageBox.Show("Four things for success: work and pray, " +
+                    //    "think and believe.\n-Norman Vincent Peale", "Sample - Button Four");
+                    break;
             }
 
             return 0;
