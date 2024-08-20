9cd0d826e710dae1b6393955be2d1b2b92d4e64b
Seeyou2000 <valentine0517@naver.com>
2024-08-20 14:31:19 +0900

    6개가 아니더라도 간섭을 검토할 수 있도록 코드 로직 리팩토링

--- Integrity Checker-MEP/Integrity Checker-MEP/ClashChecker.cs
+++ Integrity Checker-MEP/Integrity Checker-MEP/ClashChecker.cs
@@ -121,7 +121,7 @@ namespace Integrity_Checker_MEP {
 
                 
                 if (!form_setting.start) return 0; // 세팅폼에서 취소를 누를 시 종료
-
+                
                 if (form_setting.Save_image)
                 {
                     form_imageOption = new form_ImageOption();
@@ -149,6 +149,10 @@ namespace Integrity_Checker_MEP {
                     TestClashTest(); // 테스트 실행
                     GetClashResult(); // 테스트 결과 받기
                 }
+                if(form_setting.Save_image)
+                {
+                    SaveImage(); // 이미지 추출
+                }
                 if (form_setting.export_Properties) {
                     GetSpaceHeights(); // 공간별 높이 구하기
                 }
@@ -169,10 +173,6 @@ namespace Integrity_Checker_MEP {
                 }
                 if (form_setting.Save_log) {
                     SaveLog(); // 로그를 파일로 저장
-                }
-                if(form_setting.Save_image)
-                {
-                    SaveImage(); // 이미지 추출
                 }
                 form_log.UpdateLog("종료");
             }
@@ -299,76 +299,70 @@ namespace Integrity_Checker_MEP {
             return true;
         }
 
-        int _i, _j;
-        /// <summary>
-        /// 지정한 형식으로 테스트 생성
-        /// </summary>
         void MakeClashTest() {
-            _i = 0; _j = 0;
-            NewClashTest("Arch-Arch_Duplicate", 50f, ClashTestType.Duplicate);
-            _i = 5; _j = 5;
-            NewClashTest("Str-Str_Duplicate", 50f, ClashTestType.Duplicate);
+            // 모델 이름 배열 (순서가 중요)
+            string[] models = { "Arch", "COMM", "ELEC", "FIRE", "MECH", "Str" };
 
-            _i = 0; _j = 0;
-            NewClashTest("Arch-Arch_Clearance", 50f, ClashTestType.Clearance);
-            _i = 0; _j = 1;
-            NewClashTest("Arch-COMM_Clearance", 50f, ClashTestType.Clearance);
-            _i = 0; _j = 2;
-            NewClashTest("Arch-ELEC_Clearance", 50f, ClashTestType.Clearance);
-            _i = 0; _j = 3;
-            NewClashTest("Arch-FIRE_Clearance", 50f, ClashTestType.Clearance);
-            _i = 0; _j = 4;
-            NewClashTest("Arch-MECH_Clearance", 50f, ClashTestType.Clearance);
-            _i = 0; _j = 5;
-            NewClashTest("Arch-Str_Clearance", 50f, ClashTestType.Clearance);
+            // 모델이 실제로 존재하는지 확인
+            Document document = Autodesk.Navisworks.Api.Application.ActiveDocument;
+            var availableModels = document.Models
+                .Select((model, index) => new {
+                    Model = model,
+                    Name = Path.GetFileNameWithoutExtension(model.RootItem.DisplayName),
+                    Index = index
+                })
+                .Where(x => models.Contains(x.Name))  // 확장자를 제거한 이름만 선택
+                .ToList();
 
-            _i = 1; _j = 1;
-            NewClashTest("COMM-COMM_Clearance", 50f, ClashTestType.Clearance);
-            _i = 1; _j = 2;
-            NewClashTest("COMM-ELEC_Clearance", 50f, ClashTestType.Clearance);
-            _i = 1; _j = 3;
-            NewClashTest("COMM-FIRE_Clearance", 50f, ClashTestType.Clearance);
-            _i = 1; _j = 4;
-            NewClashTest("COMM-MECH_Clearance", 50f, ClashTestType.Clearance);
+            // 모델 이름과 인덱스를 딕셔너리에 저장
+            var modelDict = availableModels.ToDictionary(x => x.Name, x => x.Index);
 
-            _i = 2; _j = 2;
-            NewClashTest("ELEC-ELEC_Clearance", 50f, ClashTestType.Clearance);
-            _i = 2; _j = 3;
-            NewClashTest("ELEC-FIRE_Clearance", 50f, ClashTestType.Clearance);
-            _i = 2; _j = 4;
-            NewClashTest("ELEC-MECH_Clearance", 50f, ClashTestType.Clearance);
+            // Duplicate Tests
+            CreateTest("Duplicate", ClashTestType.Duplicate, modelDict, new (string, string)[] {
+                ("Arch", "Arch"), // Arch-Arch
+                ("Str", "Str")    // Str-Str
+            });
 
-            _i = 3; _j = 3;
-            NewClashTest("FIRE-FIRE_Clearance", 50f, ClashTestType.Clearance);
-            _i = 3; _j = 4;
-            NewClashTest("FIRE-MECH_Clearance", 50f, ClashTestType.Clearance);
-
-            _i = 4; _j = 4;
-            NewClashTest("MECH-MECH_Clearance", 50f, ClashTestType.Clearance);
-
-            _i = 5; _j = 1;
-            NewClashTest("Str-COMM_Clearance", 50f, ClashTestType.Clearance);
-            _i = 5; _j = 2;
-            NewClashTest("Str-ELEC_Clearance", 50f, ClashTestType.Clearance);
-            _i = 5; _j = 3;
-            NewClashTest("Str-FIRE_Clearance", 50f, ClashTestType.Clearance);
-            _i = 5; _j = 4;
-            NewClashTest("Str-MECH_Clearance", 50f, ClashTestType.Clearance);
-            _i = 5; _j = 5;
-            NewClashTest("Str-Str_Clearance", 50f, ClashTestType.Clearance);
-
+            // Clearance Tests
+            CreateTest("Clearance", ClashTestType.Clearance, modelDict, new (string, string)[] {
+                ("Arch", "Arch"), // Arch-Arch
+                ("Arch", "COMM"), // Arch-COMM
+                ("Arch", "ELEC"), // Arch-ELEC
+                ("Arch", "FIRE"), // Arch-FIRE
+                ("Arch", "MECH"), // Arch-MECH
+                ("Arch", "Str"),  // Arch-Str
+                ("COMM", "COMM"), // COMM-COMM
+                ("COMM", "ELEC"), // COMM-ELEC
+                ("COMM", "FIRE"), // COMM-FIRE
+                ("COMM", "MECH"), // COMM-MECH
+                ("ELEC", "ELEC"), // ELEC-ELEC
+                ("ELEC", "FIRE"), // ELEC-FIRE
+                ("ELEC", "MECH"), // ELEC-MECH
+                ("FIRE", "FIRE"), // FIRE-FIRE
+                ("FIRE", "MECH"), // FIRE-MECH
+                ("MECH", "MECH"), // MECH-MECH
+                ("Str", "COMM"),  // Str-COMM
+                ("Str", "ELEC"),  // Str-ELEC
+                ("Str", "FIRE"),  // Str-FIRE
+                ("Str", "MECH"),  // Str-MECH
+                ("Str", "Str")    // Str-Str
+            });
         }
 
-        /// <summary>
-        /// 새로운 테스트를 만들고 이름을 'ClashtestName', 공차를 'toler'로 설정합니다.
-        /// 동일한 이름의 테스트가 이미 존재하면 테스트를 만들지 않음
-        /// </summary>
-        /// <param name="ClashtestName"> 테스트의 DisplayName</param>
-        /// <param name="toler"> 테스트의 공차(단위:m)</param>
-        void NewClashTest(string ClashtestName, double toler, ClashTestType type) {
+        void CreateTest(string testTypeName, ClashTestType testType, Dictionary<string, int> modelDict, (string, string)[] pairs) {
+            foreach (var (nameA, nameB) in pairs) {
+                if (modelDict.ContainsKey(nameA) && modelDict.ContainsKey(nameB)) {
+                    int i = modelDict[nameA];
+                    int j = modelDict[nameB];
+                    string testName = $"{nameA}-{nameB}_{testTypeName}";
+                    NewClashTest(testName, 50f, testType, i, j);
+                }
+            }
+        }
+
+        void NewClashTest(string ClashtestName, double toler, ClashTestType type, int i, int j) {
             // 현재 Navisworks에 접근
             DocumentClash documentClash = Autodesk.Navisworks.Api.Application.ActiveDocument.GetClash();
-            Document document = Autodesk.Navisworks.Api.Application.ActiveDocument;
             DocumentClashTests oDCT = documentClash.TestsData;
             ClashTest ct = documentClash.TestsData.Tests.Where(x => string.Compare(x.DisplayName, ClashtestName) == 0).FirstOrDefault() as ClashTest;
             if (ct == null) {
@@ -383,10 +377,10 @@ namespace Integrity_Checker_MEP {
                 ModelItemCollection oSelA = new ModelItemCollection();
                 ModelItemCollection oSelB = new ModelItemCollection();
 
-                //assume the first and second model item are not null.
-                ModelItemEnumerableCollection oModelCollect = document.Models[0].RootItem.Children;
-                oSelA.Add(document.Models[_i].RootItem);
-                oSelB.Add(document.Models[_j].RootItem);
+                // 모델 선택
+                Document document = Autodesk.Navisworks.Api.Application.ActiveDocument;
+                oSelA.Add(document.Models[i].RootItem);
+                oSelB.Add(document.Models[j].RootItem);
                 ct.SelectionA.Selection.CopyFrom(oSelA);
                 ct.SelectionB.Selection.CopyFrom(oSelB);
                 oDCT.TestsAddCopy(ct);
@@ -394,7 +388,6 @@ namespace Integrity_Checker_MEP {
             }
             else {
                 form_log.UpdateLog($"(경고)테스트 존재 {ClashtestName}, {toler}mm, {type}");
-                //ShowMessage("[" + ClashtestName + "] is already exsist. Set other name");
             }
         }
 
